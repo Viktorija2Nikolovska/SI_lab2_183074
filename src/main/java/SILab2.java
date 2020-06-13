840c08e12159aa35aa8fe293ce8c11d37a255f13
Viktorija2Nikolovska <63191724+Viktorija2Nikolovska@users.noreply.github.com>
2020-06-13 22:13:26 +0200

    Update SILab2.java

--- src/main/java/SILab2.java
+++ src/main/java/SILab2.java
@@ -3,25 +3,25 @@ import java.util.List;
 
 public class SILab2 {
 
-    public static List<String> function(List<String> list) {
-        if (list.size() <= 0) {
-            throw new IllegalArgumentException("List length should be greater than 0");
-        }
-        List<String> numMines = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            if (!list.get(i).equals("#")) {
-                int num = 0;
-                if (i - 1 >= 0 && list.get(i - 1).equals("#")) {
-                    num++;
+   public static List<String> function(List<String> list) {//1
+        if (list.size() <= 0) {//2
+            throw new IllegalArgumentException("List length should be greater than 0");//3
+        }//4
+        List<String> numMines = new ArrayList<>();//5
+        for (int i = 0; i < list.size(); i++) {//6
+            if (!list.get(i).equals("#")) {//7
+                int num = 0;//8
+                if (i - 1 >= 0 && list.get(i - 1).equals("#")) {//9
+                    num++;//10
                 }
-                if (i + 1 < list.size() && list.get(i + 1).equals("#")) {
-                    num++;
+                if (i + 1 < list.size() && list.get(i + 1).equals("#")) {//11
+                    num++;//12
                 }
-                numMines.add(String.valueOf(num));
-            } else {
-                numMines.add(list.get(i));
+                numMines.add(String.valueOf(num));//13
+            } else {//14
+                numMines.add(list.get(i));//15
             }
-        }
-        return numMines;
-    }
+        }//16
+        return numMines;//17
+    }//18
 }
